28733e3f45b99fbc066f05392e677f617ae14f96
shafayeatt1995 <shafayetalanik@gmail.com>
2025-10-05 19:22:55 +0600

    up

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const { generateDxf, previewDxf } = require("../controllers/generate");
-const { generateDxf2, previewDxf2 } = require("../controllers/generate_bkp");
 const router = express.Router();
 
 router.post("/generate-dxf", generateDxf);
 router.post("/preview-dxf", previewDxf);
-
-router.post("/generate-dxf2", generateDxf2);
-router.post("/preview-dxf2", previewDxf2);
 
 module.exports = router;
