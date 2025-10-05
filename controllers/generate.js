090294da8afda7cebba9a54e2cc8715dfab0e63b
shafayeatt1995 <shafayetalanik@gmail.com>
2025-10-05 18:52:19 +0600

    up\\\\draw 3d line add

--- controllers/generate.js
+++ controllers/generate.js
@@ -1,71 +1,73 @@
 const Drawing = require("dxf-writer");
 
+function handleDxf(groupedPoints) {
+  const d = new Drawing();
+
+  const layers = new Set();
+  function ensureLayer(name, color = 7, type = "CONTINUOUS") {
+    if (!layers.has(name)) {
+      d.addLayer(name, color, type);
+      layers.add(name);
+    }
+  }
+
+  ensureLayer("CODE", 2);
+  ensureLayer("RL", 3);
+
+  groupedPoints.forEach((pointsArray, i) => {
+    if (!pointsArray.length) return;
+    const { layerCode, layerName, layerColor, floor, centerText } =
+      pointsArray[0];
+    ensureLayer(layerName, layerColor);
+
+    pointsArray.forEach((pt, i) => {
+      d.setActiveLayer("CODE");
+      d.drawText(pt.x, pt.y, 0.14, 0, `${pt.name}_${pt.sn}`);
+      d.setActiveLayer("RL");
+      d.drawText(pt.x, pt.y, 0.4, 0, pt.z);
+      d.setActiveLayer(layerName);
+      d.drawPoint(pt.x, pt.y, pt.z);
+    });
+
+    const allHaveId = pointsArray.every((pt) => pt.id);
+    if (allHaveId && pointsArray.length > 1) {
+      const polylineVertices = pointsArray.map((pt) => [
+        +pt.x,
+        +pt.y,
+        +pt.z || 0,
+      ]);
+
+      d.setActiveLayer(layerName);
+      d.drawPolyline3d(polylineVertices);
+    }
+
+    if (centerText) {
+      const midX =
+        pointsArray.reduce((acc, pt) => acc + +pt.x, 0) / pointsArray.length;
+      const midY =
+        pointsArray.reduce((acc, pt) => acc + +pt.y, 0) / pointsArray.length;
+      ensureLayer("TEXT");
+      d.setActiveLayer("TEXT");
+      d.drawText(
+        midX,
+        midY,
+        0.8,
+        0,
+        `${floor}${centerText}`,
+        "center",
+        "middle"
+      );
+    }
+  });
+
+  return d.toDxfString();
+}
+
 const controller = {
   async generateDxf(req, res) {
     try {
       const groupedPoints = req.body;
-      const d = new Drawing();
-
-      const layers = new Set();
-      function ensureLayer(name, color = 7, type = "CONTINUOUS") {
-        if (!layers.has(name)) {
-          d.addLayer(name, color, type);
-          layers.add(name);
-        }
-      }
-
-      ensureLayer("CODE", 2);
-      ensureLayer("RL", 3);
-
-      groupedPoints.forEach((pointsArray, i) => {
-        if (!pointsArray.length) return;
-        const { layerCode, layerName, layerColor, floor, centerText } =
-          pointsArray[0];
-        ensureLayer(layerName, layerColor);
-
-        pointsArray.forEach((pt, i) => {
-          d.setActiveLayer("CODE");
-          d.drawText(pt.x, pt.y, 0.14, 0, `${pt.name}_${pt.sn}`);
-          d.setActiveLayer("RL");
-          d.drawText(pt.x, pt.y, 0.4, 0, pt.z);
-          d.setActiveLayer(layerName);
-          d.drawPoint(pt.x, pt.y, pt.z);
-        });
-
-        const allHaveId = pointsArray.every((pt) => pt.id);
-        if (allHaveId && pointsArray.length > 1) {
-          const polylineVertices = pointsArray.map((pt) => [
-            +pt.x,
-            +pt.y,
-            +pt.z || 0,
-          ]);
-
-          d.setActiveLayer(layerName);
-          d.drawPolyline3d(polylineVertices);
-        }
-
-        if (centerText) {
-          const midX =
-            pointsArray.reduce((acc, pt) => acc + +pt.x, 0) /
-            pointsArray.length;
-          const midY =
-            pointsArray.reduce((acc, pt) => acc + +pt.y, 0) /
-            pointsArray.length;
-          ensureLayer("TEXT");
-          d.setActiveLayer("TEXT");
-          d.drawText(
-            midX,
-            midY,
-            0.8,
-            0,
-            `${floor}${centerText}`,
-            "center",
-            "middle"
-          );
-        }
-      });
-
-      const dxfString = d.toDxfString();
+      const dxfString = handleDxf(groupedPoints);
 
       res.setHeader("Content-Disposition", "attachment; filename=output.dxf");
       res.setHeader("Content-Type", "application/dxf");
@@ -75,71 +77,11 @@ const controller = {
       res.status(500).json({ message: "DXF generation failed" });
     }
   },
+
   async previewDxf(req, res) {
     try {
       const groupedPoints = req.body;
-      const d = new Drawing();
-
-      const layers = new Set();
-      function ensureLayer(name, color = 7, type = "CONTINUOUS") {
-        if (!layers.has(name)) {
-          d.addLayer(name, color, type);
-          layers.add(name);
-        }
-      }
-
-      ensureLayer("CODE", 2);
-      ensureLayer("RL", 3);
-
-      groupedPoints.forEach((pointsArray, i) => {
-        if (!pointsArray.length) return;
-        const { layerCode, layerName, layerColor, floor, centerText } =
-          pointsArray[0];
-        ensureLayer(layerName, layerColor);
-
-        pointsArray.forEach((pt, i) => {
-          d.setActiveLayer("CODE");
-          d.drawText(pt.x, pt.y, 0.14, 0, `${pt.name}_${pt.sn}`);
-          d.setActiveLayer("RL");
-          d.drawText(pt.x, pt.y, 0.4, 0, pt.z);
-          d.setActiveLayer(layerName);
-          d.drawPoint(pt.x, pt.y, pt.z);
-        });
-
-        const allHaveId = pointsArray.every((pt) => pt.id);
-        if (allHaveId && pointsArray.length > 1) {
-          const polylineVertices = pointsArray.map((pt) => [
-            +pt.x,
-            +pt.y,
-            +pt.z || 0,
-          ]);
-
-          d.setActiveLayer(layerName);
-          d.drawPolyline3d(polylineVertices);
-        }
-
-        if (centerText) {
-          const midX =
-            pointsArray.reduce((acc, pt) => acc + +pt.x, 0) /
-            pointsArray.length;
-          const midY =
-            pointsArray.reduce((acc, pt) => acc + +pt.y, 0) /
-            pointsArray.length;
-          ensureLayer("TEXT");
-          d.setActiveLayer("TEXT");
-          d.drawText(
-            midX,
-            midY,
-            0.8,
-            0,
-            `${floor}${centerText}`,
-            "center",
-            "middle"
-          );
-        }
-      });
-
-      const dxfString = d.toDxfString();
+      const dxfString = handleDxf(groupedPoints);
 
       res.setHeader("Content-Disposition", "attachment; filename=output.dxf");
       res.setHeader("Content-Type", "application/dxf");
